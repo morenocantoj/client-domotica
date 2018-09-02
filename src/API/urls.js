2ecc30888048d86a1560dcf84886d8157bc5dcb7
Jaime Moreno Cantó <jmc103@alu.ua.es>
2018-09-02 18:27:30 +0200

    URLs cambiadas a producción

--- src/API/urls.js
+++ src/API/urls.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:3000'
-export const API_URL = 'http://localhost:3000/api'
+export const BASE_URL = 'https://mighty-reef-55430.herokuapp.com/'
+export const API_URL = 'https://mighty-reef-55430.herokuapp.com/api'
